9fbf92889e880254cc250667db44857136911bce
Bob Dionne <dionne@dionne-associates.com>
2017-09-14 13:10:12 -0400

    Whitespace - format code

--- src/main/java/gov/nih/nci/export/Exporter.java
+++ src/main/java/gov/nih/nci/export/Exporter.java
@@ -26,9 +26,9 @@ import edu.stanford.protege.metaproject.api.ProjectId;
 import edu.stanford.protege.metaproject.impl.ProjectIdImpl;
 
 public class Exporter {
-	
+
 	private static final Logger log = Logger.getLogger(Exporter.class.getName());
-	
+
 	public static String USERNAME = "username";
 	public static String PWD = "password";
 	public static String HOST = "host";
@@ -38,131 +38,122 @@ public class Exporter {
 	public static String OUTPUT_FILENAME = "output_filename";
 	public static String OUTPUT_FORMAT_TYPE = "output_format_type";
 	public static String RDFXML = "RDFXML";
-	
+
 	private String config_filename = "exporter.properties";
 	private Properties config = null;
-	
-	
-	
+
 	public Exporter(String[] args) {
-		
+
 		if (args.length > 0) {
 			if (args.length == 2) {
 				if (!args[0].equals("--config")) {
 					help();
 				} else {
-					config_filename  = args[1];
+					config_filename = args[1];
 				}
-				
+
 			} else {
 				help();
 			}
-			
+
 		} else {
 			config_filename = "exporter.properties";
 		}
-		
+
 		loadProps();
 	}
-	
+
 	private void loadProps() {
 		config = new Properties();
 		try {
 			config.load(new FileInputStream(config_filename));
-		}
-		catch (FileNotFoundException fnfe) {
+		} catch (FileNotFoundException fnfe) {
 			log.log(Level.SEVERE, "Configuration file for the Exporter not found", fnfe);
 			help();
 		} catch (IOException e) {
 			log.log(Level.SEVERE, "Can't open or otherwise load file into properties", e);
 			help();
-			
+
 		}
-		
+
 	}
-	
+
 	public void run() {
 
 		try {
-			
+
 			String host = config.getProperty(HOST);
 			String port = config.getProperty(PORT);
 			String username = config.getProperty(USERNAME);
 			String password = config.getProperty(PWD);
 			String project_id = config.getProperty(PROJECT_ID);
 			String output_filename = config.getProperty(OUTPUT_FILENAME);
-			
-			String output_format_type = config.getProperty(OUTPUT_FORMAT_TYPE, RDFXML);
-			
-			LocalHttpClient client = new LocalHttpClient(username, password, host + ":" + port);
-			
-			ProjectId pid = new ProjectIdImpl(project_id);
-			
-			OpenProjectResult openProjectResult = 
-					client.openProject(pid);
-            ServerDocument serverDocument = openProjectResult.serverDocument;
-            
-            OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-            
-            VersionedOWLOntology vont = client.buildVersionedOntology(serverDocument, 
-            		man, pid);
-            
-            File f =  new File(output_filename);
-            man.setOntologyDocumentIRI(vont.getOntology(), IRI.create(f));
-            
-            final URI documentURI = man.getOntologyDocumentIRI(vont.getOntology()).toURI();
 
-            IRI documentIRI = IRI.create(documentURI);
-            
-            OWLDocumentFormat doc_format = null;
-            if (output_format_type.equals(RDFXML)) {
-            	 doc_format = new RDFXMLDocumentFormat();
-            } else {
-            	log.log(Level.SEVERE, "This output format not supported, contact developer");
-            }
-            
-            OntologySaver saver = OntologySaver.builder()
-                    .addOntology(vont.getOntology(), doc_format, documentIRI)
-                    .build();
-            saver.saveOntologies();
-            
-            log.log(Level.INFO, "Finished");
-            
-            System.exit(0);
-            
-            
-		
+			String output_format_type = config.getProperty(OUTPUT_FORMAT_TYPE, RDFXML);
+
+			LocalHttpClient client = new LocalHttpClient(username, password, host + ":" + port);
+
+			ProjectId pid = new ProjectIdImpl(project_id);
+
+			OpenProjectResult openProjectResult = client.openProject(pid);
+			ServerDocument serverDocument = openProjectResult.serverDocument;
+
+			OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+
+			VersionedOWLOntology vont = client.buildVersionedOntology(serverDocument, man, pid);
+
+			File f = new File(output_filename);
+			man.setOntologyDocumentIRI(vont.getOntology(), IRI.create(f));
+
+			final URI documentURI = man.getOntologyDocumentIRI(vont.getOntology()).toURI();
+
+			IRI documentIRI = IRI.create(documentURI);
+
+			OWLDocumentFormat doc_format = null;
+			if (output_format_type.equals(RDFXML)) {
+				doc_format = new RDFXMLDocumentFormat();
+			} else {
+				log.log(Level.SEVERE, "This output format not supported, contact developer");
+			}
+
+			OntologySaver saver = OntologySaver.builder().addOntology(vont.getOntology(), doc_format, documentIRI)
+					.build();
+			saver.saveOntologies();
+
+			log.log(Level.INFO, "Finished");
+
+			System.exit(0);
+
 		} catch (AuthorizationException | ClientRequestException e) {
 			log.log(Level.SEVERE, "User not authorized or unable to connect to the protege server", e);
 			help();
 		} catch (OWLOntologyStorageException e) {
 			log.log(Level.SEVERE, "Issue in the ontology itself", e);
-			help();			
+			help();
+
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		Exporter exp = new Exporter(args);
-		
+
 		exp.run();
-		
 
 	}
-	
+
 	private void help() {
 		StringBuffer buf = new StringBuffer();
 
-		buf.append( "Usage: export [--config <file name>] \n\n" );
-		buf.append( "Exports an ontology from a running Protégé server \n" );
-		buf.append( "Specify an optional config file to use, otherwise it looks for \n"
+		buf.append("Usage: export [--config <file name>] \n\n");
+		buf.append("Exports an ontology from a running Protégé server \n");
+		buf.append("Specify an optional config file to use, otherwise it looks for \n"
 				+ "export.properties in the directory from which the script is run \n");
-		
-		buf.append( "\n" );
-		
 
-		System.out.println( buf );
-		System.exit( 0 );
+		buf.append("\n");
+
+		System.out.println(buf);
+		System.exit(0);
 	}
 
 }
